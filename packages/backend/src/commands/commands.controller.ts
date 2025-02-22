007ed30ac3b0ce00e6b309ca73fe16e93fef31d1
ckmonish2000 <ckmonish2000@gmail.com>
2025-02-22 15:13:50 +0530

    feat: added crud commands api

--- packages/backend/src/commands/commands.controller.ts
+++ packages/backend/src/commands/commands.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CommandsService } from './commands.service';
+import CreateCommandDto from './dtos/create-command-dto';
+import UpdateCommandDto from './dtos/update-command-dto';
 
 @Controller('commands')
-export class CommandsController {}
+export class CommandsController {
+    constructor(private readonly commandsService: CommandsService) {}
+
+    @Get()
+    async getCommands() {
+        return this.commandsService.getCommands();
+    }
+
+    @Get(':id')
+    async getCommand(@Param('id') id: string) {
+        return this.commandsService.getCommand(id);
+    }
+
+    @Post()
+    async createCommand(@Body() command: CreateCommandDto) {
+        return this.commandsService.createCommand(command);
+    }
+
+    @Put(':id')
+    async updateCommand(@Param('id') id: string, @Body() command: UpdateCommandDto) {
+        return this.commandsService.updateCommand(id, command);
+    }
+
+    @Delete(':id')
+    async deleteCommand(@Param('id') id: string) {
+        return this.commandsService.deleteCommand(id);
+    }
+}
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
