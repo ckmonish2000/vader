6c32c4b93819071ecd4f10d349c5bfe1dc815859
ckmonish2000 <ckmonish2000@gmail.com>
2025-02-23 01:05:39 +0530

    chore: modified put to patch on update scripts & command

--- packages/backend/src/scripts/scripts.controller.ts
+++ packages/backend/src/scripts/scripts.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ScriptsService } from './scripts.service';
 import CreateScriptDto from './dto/create-script-dto';
 import UpdateScriptDto from './dto/update-script-dto';
@@ -8,53 +18,56 @@ import * as E from 'fp-ts/Either';
 import { throwHTTPErr } from 'src/utils';
 @Controller('scripts')
 export class ScriptsController {
-    constructor(private readonly scriptsService: ScriptsService) { }
+  constructor(private readonly scriptsService: ScriptsService) {}
 
-    @Get()
-    @UseGuards(JwtAuthGuard)
-    async getScripts(@Query() query: GetScriptsDto) {
-        const take = query && typeof query.take === 'string' ? parseInt(query.take) : query?.take;
-        const response = await this.scriptsService.getScripts({ ...query, take });
-        return response;
-    }
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getScripts(@Query() query: GetScriptsDto) {
+    const take =
+      query && typeof query.take === 'string'
+        ? parseInt(query.take)
+        : query?.take;
+    const response = await this.scriptsService.getScripts({ ...query, take });
+    return response;
+  }
 
-    @Get(':id')
-    @UseGuards(JwtAuthGuard)
-    async getScript(@Param('id') id: string) {
-        const script = await this.scriptsService.getScript(id);
-        if (E.isLeft(script)) {
-            throwHTTPErr(script.left);
-        }
-        return script.right;
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  async getScript(@Param('id') id: string) {
+    const script = await this.scriptsService.getScript(id);
+    if (E.isLeft(script)) {
+      throwHTTPErr(script.left);
     }
+    return script.right;
+  }
 
-    @Post()
-    @UseGuards(JwtAuthGuard)
-    async createScript(@Body() script: CreateScriptDto) {
-        const newScript = await this.scriptsService.createScript(script);
-        if (E.isLeft(newScript)) {
-            throwHTTPErr(newScript.left);
-        }
-        return newScript.right;
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async createScript(@Body() script: CreateScriptDto) {
+    const newScript = await this.scriptsService.createScript(script);
+    if (E.isLeft(newScript)) {
+      throwHTTPErr(newScript.left);
     }
+    return newScript.right;
+  }
 
-    @Put(':id')
-    @UseGuards(JwtAuthGuard)
-    async updateScript(@Param('id') id: string, @Body() script: UpdateScriptDto) {
-        const updatedScript = await this.scriptsService.updateScript(id, script);
-        if (E.isLeft(updatedScript)) {
-            throwHTTPErr(updatedScript.left);
-        }
-        return updatedScript.right;
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  async updateScript(@Param('id') id: string, @Body() script: UpdateScriptDto) {
+    const updatedScript = await this.scriptsService.updateScript(id, script);
+    if (E.isLeft(updatedScript)) {
+      throwHTTPErr(updatedScript.left);
     }
+    return updatedScript.right;
+  }
 
-    @Delete(':id')
-    @UseGuards(JwtAuthGuard)
-    async deleteScript(@Param('id') id: string) {
-        const deletedScript = await this.scriptsService.deleteScript(id);
-        if (E.isLeft(deletedScript)) {
-            throwHTTPErr(deletedScript.left);
-        }
-        return deletedScript.right;
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  async deleteScript(@Param('id') id: string) {
+    const deletedScript = await this.scriptsService.deleteScript(id);
+    if (E.isLeft(deletedScript)) {
+      throwHTTPErr(deletedScript.left);
     }
+    return deletedScript.right;
+  }
 }
